ae133a7b807b771660bae1e869f8354588d1a462
Tareq <razzbabut@gmail.com>
2024-11-17 21:03:14 +0600

    complete parser, request and response object in express

---
+++ dist/app/app.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+// const express = require('express')
+const app = (0, express_1.default)();
+// parsers
+app.use(express_1.default.json());
+app.use(express_1.default.text());
+app.get('/', (req, res) => {
+    res.send('Hello level-1 web developers!');
+});
+app.post('/', (req, res) => {
+    console.log(req.body);
+    // res.send('okay got it')
+    res.json({
+        message: "Successfully received data"
+    });
+});
+exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,21 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 // const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// parsers
+app.use(express.json())
+app.use(express.text())
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello level-1 web developers!')
+})
+
+app.post('/', (req: Request, res: Response) => {
+    console.log(req.body);
+    // res.send('okay got it')
+    res.json({
+        message : "Successfully received data"
+    })
 })
 
 export default app;
